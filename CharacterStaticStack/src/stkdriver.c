064dc4731d2601db948ec8e5ffc7bd883c61e766
Shereen Khoja <shereen@pacificu.edu>
2021-08-24 16:55:12 -0700

    Initial Commit

---
+++ CharacterStaticStack/src/stkdriver.c
@@ -0,0 +1,30 @@
+/**************************************************************************
+ File name:  stkdriver.c
+ Author:     Computer Science, Pacific University
+ Date:			 10.11.17
+ Class:			 CS300
+ Assignment: Static Generic Stack
+ Purpose:    Test driver for a static stack of generic elements
+ *************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/stk.h"
+
+/**************************************************************************
+ Function: 	 	main
+
+ Description: test all the functionality of the stack
+
+ Parameters:	none
+
+ Returned:	 	Exit Status
+ *************************************************************************/
+
+int main() {
+	Stack sTheStack;
+	char data;
+
+	return EXIT_SUCCESS;
+}
